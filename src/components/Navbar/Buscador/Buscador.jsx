b99b586b8794f34009fc130672e20000d01a2adb
sofi3784 <sofi.3784@gmail.com>
2023-02-19 20:21:51 -0300

    navbar

---
+++ src/components/Navbar/Buscador/Buscador.jsx
@@ -0,0 +1,10 @@
+const Buscador = () => {
+    return (
+    <form class="d-flex" role="buscar">
+        <input class="form-control me-2" type="buscar" placeholder="Buscar" aria-label="Buscar"/>
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+    </form>
+    );
+}
+
+export default Buscador;
---
+++ src/components/Navbar/Carrito/Carrito.jsx
@@ -0,0 +1,10 @@
+const Carrito = () => {
+    return (
+        <div>
+            <button className="btn btn-dark">Carrito</button>
+            <span></span>
+        </div>
+    );
+}
+
+export default Carrito;
